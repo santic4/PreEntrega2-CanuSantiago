5913d1bf3ea13bcf422ba9ac0ff7c23452f3d47a
Santiago Cañu <santisv4@gmail.com>
2023-06-13 16:00:59 -0300

    CanuSantiagoPreEntrega3

--- script.js
+++ script.js
@@ -1,169 +1,4 @@
-/*
-//Funciones
-
-function CalcularPrecioTotal() {
-  let precioFinal = 0;
-  carrito.forEach((e) => {
-    precioFinal += e.precio;
-  });
-  precioFinal = precioFinal.toFixed(3);
-  return precioFinal;
-}
-
-
-function verificarEnvioGratis(precioFinal) {
-  if (precioFinal > 50.000) {
-    alert('Superaste los $50.000. ¡El envío es gratis!');
-  }
-}
-
-class Producto {
-  constructor(id, nombre, marca, precio, categoria, codigo, stock) {
-    this.id = id;
-    this.nombre = nombre;
-    this.marca = marca;
-    this.precio = precio;
-    this.categoria = categoria;
-    this.codigo = codigo;
-    this.stock = stock;
-  }
-}
-
-const libroElEternauta = new Producto(1, "ElEternauta", "Héctor Oesterheld", 5.599, "Libros", "LIB-1", 100);
-const camisetaAdidas = new Producto(2, "camisetaAdidas", "Adidas", 11.559, "Indumentaria", "I-AD-01", 30);
-const auricularesBluetooth = new Producto(3, "auricularesBluetooth", "Sony", 23.999, "Electronica", "E-AU-01", 15);
-const zapatillasDeportivas = new Producto(4, "zapatillasDeportivas", "Adidas", 28.159, "Indumentaria", "I-ZA-01", 3);
-const juegoMesaTEG = new Producto(5, "TEG", "Ruibal", 4.359, "Juego de Mesa", "J-M-01", 10);
-
-//arrayProductos
-const arrayProductos = [libroElEternauta, camisetaAdidas, auricularesBluetooth, zapatillasDeportivas, juegoMesaTEG];
-
-// Carrito de compras
-const carrito = [];
-
-let Comprando = true;
-
-// Interaccion con el cliente
-const respuesta = prompt('¿Desea agregar un producto al carrito? (Sí/No)');
-
-alert(`Si tu pedido supera los $50.000, ¡te regalamos el envio!`)
-
-function InteraccionPorElCarrito() {
-  while (Comprando) {
-    if (respuesta.toLowerCase() === 'sí' || respuesta.toLowerCase() === 'si') {
-      
-      let opciones = 'Productos disponibles:\n';
-
-      // Filtrar los productos disponibles por stock
-      const productosDisponibles = arrayProductos.filter((e) => e.stock > 0);
-
-      // Mostrar solo los productos disponibles en las opciones
-      productosDisponibles.forEach((e) => {
-        opciones += `ID: ${e.id}, Producto: ${e.nombre}, Precio: $${e.precio}\n`;
-      });
-
-      // Solicitar al cliente que elija un producto
-      const opcionProducto = parseInt(prompt(opciones + 'Ingrese el número del producto que desea agregar al carrito:'));
-
-      // Buscar el producto por su ID en la lista de productos disponibles
-      const productoElegido = productosDisponibles.find((e) => e.id === opcionProducto);
-
-      if (productoElegido) {
-        // Agregar el producto al carrito
-        carrito.push(productoElegido);
-
-        // Restar el stock del producto seleccionado
-        productoElegido.stock -= 1;
-        alert(`${productoElegido.nombre} ha sido agregado al carrito.`);
-      } else {
-        alert('Stock insuficiente, elije otro producto.');
-      }
-
-      // Preguntar si desea agregar otro producto
-      const respuestaOtroProducto = prompt('¿Desea agregar otro producto? (Sí/No)');
-      Comprando = (respuestaOtroProducto.toLowerCase() === 'sí' || respuestaOtroProducto.toLowerCase() === 'si');
-    } else {
-      // Salir del bucle si no desea agregar ningun producto
-      Comprando = false;
-    }
-  }
-
-  // Calcular el precio final
-  precioFinal = CalcularPrecioTotal();
-
-  // Verificar si el envio es gratis
-  verificarEnvioGratis(precioFinal);
-
-
-  // Mensaje del carrito y precio final
-  let mensajeCarrito = 'Carrito de compras:\n';
-  carrito.forEach((e) => {
-    mensajeCarrito += `Producto: ${e.nombre}, Precio: $${e.precio}\n`;
-  });
-  mensajeCarrito += `Precio final: $${precioFinal}`;
-
-  // Mostrar el carrito de compras y precio final
-  alert(mensajeCarrito);
-  console.table(carrito);
-  console.log(`Precio Total: $${precioFinal}`);
-}
-
-InteraccionPorElCarrito();
-*/
-
-/*
-
-mostrarBotonCrear = document.getElementById("btnShowCrear")
-mostrarBotonCrear.addEventListener("click", function() {
-  document.getElementById("Crear").classList.add("show");
-})
-
-const showPass = document.querySelector("#showPass")
-
-function verPass(){
-  let userPassword = document.getElementById("passwordCrear")
-
-  if (userPassword.type == "password"){
-    userPassword.type = "text";
-  }else{
-    userPassword.type = "password"
-  }
-}
-
-showPass.addEventListener("mouseover", verPass)
-showPass.addEventListener("mouseout", verPass)
-
-
-// Obtener referencias a los elementos del DOM
-const emailInput = document.getElementById('emailLogin');
-const passwordInput = document.getElementById('passwordLogin');
-const submitButton = document.getElementById('submitLogin');
-const btnMostrarFormulario = document.getElementById('btnMostrarFormulario');
-const contenedorLogin = document.getElementById('Login');
-
-// Agregar evento de clic al botón "Iniciar sesión"
-btnMostrarFormulario.addEventListener('click', function() {
-  contenedorLogin.style.display = 'block';
-});
-
-// Agregar un evento de escucha para el botón de inicio de sesión
-submitButton.addEventListener('click', function(event) {
-  event.preventDefault(); // Evitar el comportamiento predeterminado del formulario
-
-  // Obtener los valores de los campos de entrada
-  const email = emailInput.value;
-  const password = passwordInput.value;
-
-  // Realizar la lógica de inicio de sesión
-  if (email && password) {
-    // Aquí puedes realizar la validación del usuario y la contraseña con tu lógica de backend
-
-    // Simplemente mostramos un mensaje en la consola para este ejemplo
-    console.log('Inicio de sesión exitoso');
-  } else {
-    console.log('Por favor, complete todos los campos');
-  }
-});*/
+                                                           /* USUARIO Y LOCAL STORAGE*/
 
 const btnClickCrear = document.getElementById("btnShowCrear");
 const btnClickLogin = document.getElementById("btnMostrarFormulario");
@@ -192,4 +27,334 @@ btnSalirLogin.addEventListener("click", (event) => {
   event.preventDefault();
   contenedorLogin.style.display = "none";
 });
-//ACA
+
+// Obtener elementos del formulario
+const formCrear = document.getElementById('crearFormulario');
+const signUpButton = document.getElementById('signUp');
+
+// Función para manejar el evento de envío del formulario
+function eventoSignUp(event) {
+  event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+  // Obtener los valores de los campos del formulario
+  const email = document.getElementById('emailCrear').value;
+  const password = document.getElementById('passwordCrear').value;
+  const confirmPassword = document.getElementById('confirmPasswordCrear').value;
+  const nombre = document.getElementById('nameCrear').value;
+  const apellido = document.getElementById('apellidoCrear').value;
+  const fechaNacimiento = document.getElementById('fechaNacimientoCrear').value;
+  const dni = document.getElementById('dniCrear').value;
+  const telefono = document.getElementById('telefono').value;
+
+  // Verificar que los campos obligatorios estén completos
+ 
+  !email || !password || !confirmPassword || !nombre || !apellido || !fechaNacimiento || !dni || !telefono 
+  && (alert('Por favor, complete todos los campos obligatorios. (*), return'))
+
+  // Verificar que las contraseñas coincidan
+  if (password !== confirmPassword) {
+    alert('Las contraseñas no coinciden. Por favor, verifique.')
+    return
+  }
+
+  // Crear un objeto con los datos del usuario
+  const usuario = {
+    email: email,
+    password: password,
+    nombre: nombre,
+    apellido: apellido,
+    fechaNacimiento: fechaNacimiento,
+    dni: dni,
+    telefono: telefono
+  };
+
+  // Obtener el arreglo de usuarios del LocalStorage
+  let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+  // Generar un ID único para el usuario
+  const userId = generateUserId();
+
+  // Agregar el ID al objeto del usuario
+  usuario.id = userId;
+
+  // Agregar el usuario al arreglo de usuarios
+  usuarios.push(usuario);
+
+  // Guardar el arreglo de usuarios actualizado en el LocalStorage
+  localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+  // Limpiar los campos del formulario
+  formCrear.reset();
+
+  // Mostrar un mensaje de éxito
+  alert('Usuario registrado correctamente.');
+
+  
+}
+
+// Función para generar un ID único para el usuario
+function generateUserId() {
+  // Obtener el último ID utilizado del LocalStorage
+  let lastUserId = localStorage.getItem('lastUserId');
+
+  // Si no hay ningún ID almacenado, comenzar desde 1
+  if (!lastUserId) {
+    lastUserId = 0;
+  }
+
+  // Incrementar el ID en 1 para el nuevo usuario
+  const newUserId = parseInt(lastUserId) + 1;
+
+  // Guardar el nuevo ID en el LocalStorage
+  localStorage.setItem('lastUserId', newUserId);
+
+  return newUserId;
+}
+
+// Obtener elementos del formulario
+
+const signInButton = document.getElementById('signIn');
+
+// Función para manejar el evento de inicio de sesión
+function eventoSignIn(event) {
+  event.preventDefault(); // Prevenir el envío del formulario por defecto
+
+  // Obtener los valores de los campos del formulario
+  const email = document.getElementById('emailLogin').value;
+  const password = document.getElementById('passwordLogin').value;
+
+  // Verificar que los campos obligatorios estén completos
+  if (!email || !password) {
+    alert('Por favor, complete todos los campos obligatorios.');
+    return;
+  }
+
+  // Obtener el arreglo de usuarios del LocalStorage
+  let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+  // Buscar el usuario por email y contraseña
+  const usuario = usuarios.find((u) => u.email === email && u.password === password);
+
+  // Verificar si el usuario existe
+  usuario ?
+  (btnShowCrear.style.display = 'none',
+  btnMostrarFormulario.style.display = 'none',
+  carritoIcon.style.display = 'block',
+  carritoContainer.style.display = 'block',
+  localStorage.setItem('carrito', JSON.stringify(carrito))) :
+  alert('Usuario no encontrado, o contraseña/email incorrectos.');
+
+  usuario 
+
+  contenedorLogin.style.display = "none";
+
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+
+  actualizarCarrito ()
+}
+
+// Agregar un evento de escucha al botón "Enviar"
+signInButton.addEventListener('click', eventoSignIn);
+
+
+// Agregar un evento de escucha al botón "Enviar"
+signUpButton.addEventListener('click', eventoSignUp);
+
+
+
+                                                            /*PRODUCTOS*/
+
+// Clase Producto
+class Producto {
+  constructor(id, nombre, marca, precio, categoria, codigo, stock) {
+    this.id = id;
+    this.nombre = nombre;
+    this.marca = marca;
+    this.precio = precio;
+    this.categoria = categoria;
+    this.codigo = codigo;
+    this.stock = stock;
+  }
+}
+
+// Crear instancias de productos
+const libroElEternauta = new Producto(1, "El Eternauta", "Héctor Oesterheld", 5.599, "Libros", "LIB-1", 7);
+const camisetaAdidas = new Producto(2, "Remera Adidas", "Adidas", 11.559, "Indumentaria", "I-AD-01", 30);
+const auricularesBluetooth = new Producto(3, "Auriculares Bluetooth", "Sony", 23.999, "Electronica", "E-AU-01", 15);
+const zapatillasDeportivas = new Producto(4, "Zapatillas", "Adidas", 28.159, "Indumentaria", "I-ZA-01", 3);
+const juegoMesaTEG = new Producto(5, "TEG", "Ruibal", 4.359, "Juego de Mesa", "J-M-01", 10);
+
+// Array de productos
+const arrayProductos = [libroElEternauta, camisetaAdidas, auricularesBluetooth, zapatillasDeportivas, juegoMesaTEG];
+
+// Función para crear una card de producto
+function crearCardProducto(producto) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const title = document.createElement('h3');
+  title.textContent = producto.nombre;
+  card.appendChild(title);
+
+  const brand = document.createElement('p');
+  brand.textContent = 'Marca: ' + producto.marca;
+  card.appendChild(brand);
+
+  const price = document.createElement('p');
+  price.textContent = 'Precio: $' + producto.precio;
+  card.appendChild(price);
+
+  const stock = document.createElement('p');
+  stock.textContent = 'Stock: ' + producto.stock;
+  card.appendChild(stock);
+
+  const addToCartBtn = document.createElement('button');
+
+  addToCartBtn.textContent = 'Agregar al carrito';
+
+  card.classList.add("Carta")
+
+  addToCartBtn.addEventListener('click', () => {
+
+  //Restar uno al stock de la card
+
+    producto.stock > 0
+    && (agregarAlCarrito(producto), producto.stock -= 1,  stock.textContent = 'Stock: ' + producto.stock)
+    producto.stock == 0 && (addToCartBtn.innerHTML = '<h4>Sin stock</h4>')
+
+  });
+  card.appendChild(addToCartBtn);
+
+  return card;
+}
+
+
+
+// Obtener el contenedor de productos y carrito
+const productosContainer = document.getElementById('productosContainer');
+const carritoContainer = document.getElementById('carritoContainer');
+
+// Crear las cards de productos y agregarlas al contenedor de productos
+arrayProductos.forEach((producto) => {
+  const card = crearCardProducto(producto);
+  productosContainer.appendChild(card);
+});
+
+                                                                 /*CARRITO*/
+
+// Array para almacenar los productos agregados al carrito
+let carrito = [];
+
+
+// Función para actualizar el contenido del carrito
+function actualizarCarrito() {
+  const carritoItems = document.getElementById('carritoItems');
+  const carritoTotal = document.getElementById('carritoTotal');
+
+  // Vaciar carrito antes de actualizarlo
+  carritoItems.innerHTML = '';
+
+  let total = 0;
+
+  carrito.forEach((producto) => {
+    const item = crearCardProductoCarrito(producto);
+    carritoItems.appendChild(item);
+    total += producto.precio;
+  });
+
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+
+  carritoTotal.textContent = '$' + total.toFixed(2);
+
+}
+
+// Obtener el elemento del icono del carrito
+const carritoIcon = document.getElementById('carritoIcon');
+
+carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+// Evento al hacer clic en el icono del carrito
+carritoIcon.addEventListener('click', toggleCarrito);
+
+// Variable para controlar el estado del carrito
+let carritoVisible = false;
+
+// Función para mostrar/ocultar el carrito
+function toggleCarrito() {
+  showCarrito = document.getElementById('carritoContainer')
+  showCarrito.style.display = 'block';
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+// Función para agregar un producto al carrito y mostrarlo
+function agregarAlCarrito(producto) {
+  carrito.push(producto);
+  actualizarCarrito();
+
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+
+  !carritoVisible && toggleCarrito()
+
+}
+
+// Evento al hacer clic en el icono del carrito
+carritoIcon.addEventListener('click', toggleCarrito);
+
+localStorage.setItem('carrito', JSON.stringify(carrito))
+
+// Agregar evento al botón de vaciar carrito
+const vaciarCarritoBtn = document.getElementById('vaciarCarrito');
+vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+
+// Función para eliminar un producto del carrito
+function eliminarDelCarrito(producto) {
+
+  const index = carrito.indexOf(producto);
+ 
+  index > -1 && (carrito.splice(index, 1), actualizarCarrito())
+}
+
+// Función para crear una card de producto en el carrito
+function crearCardProductoCarrito(producto) {
+  
+  const card = document.createElement('div');
+  card.classList.add('card-carrito');
+
+  const title = document.createElement('h4');
+  title.textContent = producto.nombre;
+  card.appendChild(title);
+  title.style.width = "200px"
+
+  const price = document.createElement('p');
+  price.textContent = '$' + producto.precio;
+  card.appendChild(price);
+
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Eliminar';
+  removeBtn.addEventListener('click', () => {
+    eliminarDelCarrito(producto);
+  });
+  card.appendChild(removeBtn);
+
+  return card;
+}
+
+// Función para agregar un producto al carrito
+function agregarAlCarrito(producto) {
+  carrito.push(producto);
+  actualizarCarrito();
+}
+
+const btnEsconder = document.getElementById('esconderCarrito')
+const contCarrito = document.getElementById('carritoContainer')
+
+btnEsconder.onclick = () => { contCarrito.style.display = 'none'};
+
+
+// Función para vaciar el carrito
+function vaciarCarrito() {
+  carrito = [];
+  actualizarCarrito();
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+}
